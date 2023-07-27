e5e5653e8eb50c2f22d084bf5d458a04d5ab2e08
天边的生活 <104612354+tianbiandeshenghuo@users.noreply.github.com>
2023-07-27 15:31:43 +0000

    init commit

---
+++ cf1849/test.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+  freopen("t.in", "w", stdout);
+  cout << "1\n";
+  int n = 3e5;
+  cout << n << ' ' << 1 << '\n';
+  while (n--){
+    int x = 1e9;
+    cout << x << ' ';
+  }
+  return 0;
+}
