aed1e3fc1e3860c804a26407558a45312af405cb
天边的生活 <104612354+tianbiandeshenghuo@users.noreply.github.com>
2023-09-27 05:57:45 +0000

    update cf1878

--- cf1878/E_Iva_Pav.cpp
+++ cf1878/E_Iva_Pav.cpp
@@ -1,56 +1,53 @@
 #include<bits/stdc++.h>
+#define int long long
 
 using namespace std;
 const int MAXN = 2e5 + 5;
-int n, q, a[MAXN], b[MAXN], c[MAXN];
+int n, q, a[MAXN], tr[4 * MAXN];
+void js(int id, int l, int r){
+  if (l == r){
+    tr[id] = a[l];
+    return ;
+  }
+  int mid = (l + r) >> 1;
+  js(id * 2, l, mid);
+  js(id * 2 + 1, mid + 1, r);
+  tr[id] = (tr[id * 2] & tr[id * 2 + 1]);
+}
+int find(int id, int l, int r, int ql, int qr){
+  if (l > qr || r < ql){
+    return (1ll << 30) - 1;
+  }else if (ql <= l && r <= qr){
+    return tr[id];
+  }
+  int mid = (l + r) >> 1;
+  return find(id * 2, l, mid, ql, qr) & find(id * 2 + 1, mid + 1, r, ql, qr);
+}
 void Solve(){
   cin >> n;
-  int last = 1;
-  b[n + 1] = 1e9 + 1;
-  c[n + 1] = n;
   for (int i = 1; i <= n; i++){
-    c[i] = n;
     cin >> a[i];
-    b[i] = (a[i] & a[i - 1]);
-    if (!b[i]){
-      for (int j = last; j < i; j++){
-        c[j] = i;
-      }
-      last = i + 1;
-    }
   }
-  for (int i = 1; i <= n; i++){
-    cerr << c[i] << " \n"[i == n];
-  }
-  for (int i = 1; i <= n; i++){
-    cerr << b[i] << " \n"[i == n];
-  }
+  js(1, 1, n);
   cin >> q;
   while (q--){
-    int l, k;
+    int l, r = n, k;
     cin >> l >> k;
-    bool flag = 1;
-    for (int i = c[l + 1]; i >= l; i--){
-      //cout << i << ' ' << b[i] << '\n';
-      if (b[i] >= k){
-        flag = 0;
-        cout << i << ' ';
-        break;
-      }else if (i == l && b[l] >= k){
-        cout << i << ' ';
-        flag = 0;
-        break;
+    int tmp = l;
+    while (l < r){
+      int mid = (l + r + 1) >> 1;
+      if (find(1, 1, n, tmp, mid) >= k){
+        l = mid;
+      }else {
+        r = mid - 1;
       }
     }
-    if (flag){
-      cout << "-1 ";
-    }
+    cout << (find(1, 1, n, tmp, r) < k ? -1 : r) << ' ';
   }
   cout << '\n';
 }
-int main(){
-  //ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-  a[0] = (1ll << 30) - 1;
+signed main(){
+  ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
   int t;
   cin >> t;
   while (t--){
