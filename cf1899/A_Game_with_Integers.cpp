ade68f54f9b0858f8e64a0672662c5f1c569a99f
天边的生活 <104612354+tianbiandeshenghuo@users.noreply.github.com>
2023-11-17 16:58:57 +0000

    update

--- cf1899/A_Game_with_Integers.cpp
+++ cf1899/A_Game_with_Integers.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 void Solve(){
-
+  int n;
+  cin >> n;
+  cout << (n % 3 == 0 ? "Second" : "First") << '\n';
 }
 signed main(){
   ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
--- cf1899/test.cpp
+++ cf1899/test.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 void Solve(){
-
+  
 }
 signed main(){
   ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
